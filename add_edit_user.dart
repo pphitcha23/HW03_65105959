0845dd64b07bf9ba558a49b5d06714777b8e3102
pphitcha23 <phitchaphonpansuk@gmail.com>
2024-08-31 22:04:51 +0700

    Add files via upload

---
+++ add_edit_user.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import '../models/users.dart';
+
+class AddEditUser extends StatefulWidget {
+  final Users? user;
+
+  const AddEditUser({this.user, Key? key}) : super(key: key);
+
+  @override
+  State<AddEditUser> createState() => _AddEditUserState();
+}
+
+class _AddEditUserState extends State<AddEditUser> {
+  final _formKey = GlobalKey<FormState>();
+  late Users _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _user = widget.user ?? Users();
+  }
+
+  Widget buildInputField(String label, String? initialValue, Function(String?) onSave, IconData icon) {
+    return Focus(
+      child: Builder(
+        builder: (context) {
+          final isFocused = Focus.of(context).hasFocus;
+          return TextFormField(
+            initialValue: initialValue,
+            obscureText: label == 'Password' ? true : false, // ซ่อน password โดยไม่มีไอคอน
+            decoration: InputDecoration(
+              labelText: label,
+              labelStyle: TextStyle(color: isFocused ? const Color.fromARGB(255, 77, 77, 77) : Colors.grey),
+              prefixIcon: Icon(icon, color: isFocused ? const Color.fromARGB(255, 88, 88, 88) : Colors.grey),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: const Color.fromARGB(255, 92, 92, 92)),
+              ),
+            ),
+            onSaved: onSave,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildGenderField() {
+    return DropdownButtonFormField<String>(
+      value: _user.gender,
+      decoration: InputDecoration(
+        labelText: 'Gender',
+        prefixIcon: Icon(Icons.person, color: Colors.grey),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: const Color.fromARGB(255, 243, 33, 243)),
+        ),
+      ),
+      items: ['None', 'Male', 'Female']
+          .map((label) => DropdownMenuItem(
+                child: Text(label),
+                value: label,
+              ))
+          .toList(),
+      onChanged: (value) => setState(() {
+        _user.gender = value;
+      }),
+    );
+  }
+
+  Future<void> saveUser() async {
+    _formKey.currentState?.save();
+    Navigator.pop(context, _user); // ส่งข้อมูลผู้ใช้กลับไปยังหน้าหลัก
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'User Form',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: const Color.fromARGB(255, 243, 33, 208),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              buildInputField('Full Name', _user.fullname, (value) => _user.fullname = value, Icons.person),
+              buildInputField('Email', _user.email, (value) => _user.email = value, Icons.email),
+              buildInputField('Password', _user.password, (value) => _user.password = value, Icons.lock),
+              buildGenderField(),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: saveUser,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 243, 33, 208),
+                ),
+                child: const Text('Save', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ config.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_application_8/models/users.dart';
+
+class Configure {
+  static const server = "100.117.6.55:80";
+  static Users login = Users();
+}
